286287a7e9581c3baa04ceb4e88777f65bca4d67
m_shadkam <m.shadkam@isc.co.ir>
2024-07-10 14:14:10 +0330

    add button component

---
+++ projects/cl/src/lib/button/button.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+import {CommonModule} from "@angular/common";
+
+@Component({
+  selector: 'ngCl-button',
+  templateUrl: './button.component.html',
+  standalone: true,
+  imports: [
+    CommonModule
+  ],
+})
+export class NgClButtonComponent {
+}
